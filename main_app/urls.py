2229e991b0a6540532ee3b03b31028513b25d953
dshamany <dshamany@gmail.com>
2019-12-20 12:58:17 -0800

    Fix detail view content displaying with new line

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_index, name='index'),
-    path('/profile', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('about/', views.about, name='about'),
     path('signup', views.signup, name='signup'),
     
