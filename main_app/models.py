b7d04ce115c1651458fdc7bbc6f77c7edff9ea85
Nik Bagramyan <nikbagramyan@Nik-Bagramyan-MacBook-Pro.local>
2019-12-20 16:56:08 -0800

    fixes gitignore

--- main_app/models.py
+++ main_app/models.py
@@ -11,15 +11,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-<<<<<<< HEAD
-    content = models.TextField()
-    photoUrl = models.CharField(max_length=100)
-    videoUrl = models.CharField(max_length=100)
-=======
     content = models.TextField(blank=True)
     imageUrl = models.CharField(max_length=100, blank=True)
     videoUrl = models.CharField(max_length=100, blank=True)
->>>>>>> 66c4aaf8e06751dc0a4c32a4e75efcf81023e8f9
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # sources = 
     datePublished = models.DateTimeField(auto_now_add=True)
+    
